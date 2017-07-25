6b302cd9149841a0a0c9e3da7e987541069bc40e
Juan Pineda <jfpineda84@gmail.com>
2017-07-25 19:48:13 -0400

    updated migration code

--- db/migrate/20170725234517_add_image_to_pages.rb
+++ db/migrate/20170725234517_add_image_to_pages.rb
@@ -1,4 +1,9 @@
 class AddImageToPages < ActiveRecord::Migration[5.1]
-  def change
-  end
+    def self.up
+     add_attachment :friends, :avatar
+   end
+
+   def self.down
+     remove_attachment :friends, :avatar
+   end
 end
